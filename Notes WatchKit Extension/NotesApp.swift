4e38a1aebbbcaa4c8d25719da3872b59e75ad5a7
AlexBQ7 <alejandro.barreto.quintana@gmail.com>
2022-12-26 15:26:15 -0600

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by David Alejandro Ruiz Hernandez on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
